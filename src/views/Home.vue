af5deaaccb09d80eb4858af145cd7ea75b62d5f1
favour vivian woka <vivianemma97@gmail.com>
2020-04-28 02:12:53 +0100

    Styling here-page Ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container-fluid p-0 m-0">
+    <div class="row p-0 m-0 justify-content-around align-items-center">
+      <div class="col-12 text-center navbar-logo">
+        <div class="fixed text-center pt-1">
+          Covid-19
+          <img src="../assets/covid-logo.png" alt />
+          Update
+        </div>
+      </div>
+      <div class="col-12 hero-page">
+        <div class="container text-center hero-page-content">
+          <div class="row hero-page-content-details">
+            <div class="col-lg-4 col-sm-12 col-md-4">
+              <p>Total Case</p>
+            </div>
+            <div class="col-lg-4 col-sm-12 col-md-4">
+              <p>Recovered Case</p>
+            </div>
+            <div class="col-lg-4 col-sm-12 col-md-4">
+              <p>Death Case</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  // data() {
+  //   total: [];
+  // },
+  // methods: {},
+  components: {}
 };
 </script>
+<style lang="scss" scoped>
+.navbar-logo {
+  background: rgb(255, 255, 255);
+  height: 9vh;
+  text-align: center;
+  font-size: 20px;
+  box-shadow: 0 4px 0 rgb(138, 138, 138);
+  img {
+    width: 4%;
+    text-align: center;
+  }
+}
+.hero-page {
+  background: linear-gradient(rgb(203, 59, 57), rgb(203, 59, 57));
+  // background-position: 50% 50%;
+  // background-repeat: no-repeat;
+  // background-size: 345px 412px;
+  width: 100%;
+  height: 91.1vh;
+  // background-attachment: fixed;
+  // color: #fff;
+  .hero-page-content {
+    background: url("../assets/map.png");
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+    background-size: 345px 412px;
+    width: 100%;
+    height: 91.1vh;
+    background-attachment: fixed;
+    color: #fff;
+    .hero-page-content-details {
+      height: 91.1vh;
+      justify-content: space-around;
+      align-items: center;
+    }
+  }
+}
+</style>
